6ebfc78f8df595d37e3aaac7b1bb8d7f27694b16
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-05-11 21:16:52 +0100

    Add new files

--- db/migrate/20230511105525_add_number_of_posts_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberOfPostsToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :posts_counter, :integer
-  end
-end
--- db/migrate/20230511110850_add_number_of_likes_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberOfLikesToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :like_counter, :integer
-  end
-end
