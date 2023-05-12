5b6d550956530288fba0d9f6ea15d21ce62e6a7c
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-05-12 15:04:37 +0100

    Correct comments table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+    
+        belongs_to :author, foreign_key: :author_id, class_name: 'User'
+        belongs_to :post, class_name: 'Post'
+      
+        after_save :update_post_Comment_Counter
+      
+        def update_post_Comment_Counter
+          post.increment!(:CommentsCounter)
+        end
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments
-  has_many :likes
+  belongs_to :author, foreign_key: :author_id, class_name: 'User', counter_cache: :postscounter
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def update_user_post_counter
+    author.increment!(:postscounter)
+  end
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
