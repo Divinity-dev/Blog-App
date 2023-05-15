7be4e63824fce7564d27eb59ea29c08475c684c9
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-05-15 18:12:26 +0100

    fixed linter errors

--- app/controllers/posts_contrroller.rb
+++ app/controllers/posts_contrroller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
-    def index
-      @user = User.find(params[:user_id])
-      @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 5)
-    end
-  
-    def show
-      @user = User.find(params[:user_id])
-      @post = @user.posts.find(params[:id])
-      @comments = @post.comments.includes(:author).paginate(page: params[:page], per_page: 10)
-      @CommentsCount = @post.comments.count
-      @LikesCount = @post.likes.size
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 5)
   end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments.includes(:author).paginate(page: params[:page], per_page: 10)
+    @comments_count = @post.comments.count
+    @likes_count = @post.likes.size
+  end
+end
--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-    def index
-      @users = User.all
-      @posts = Post.limit(3)
-    end
-  
-    def show
-      @user = User.find(params[:id])
-    end
+  def index
+    @users = User.all
+    @posts = Post.limit(3)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
