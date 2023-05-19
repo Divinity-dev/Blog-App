a5867a43f7e907dd5680cf74230be8ed615e03b8
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-05-20 00:30:30 +0100

    Add forms for posts and comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :current_user
     def current_user
         @current_user = User.first
       end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "users#index"
 
-  resources :users, only: [:index, :show] do
-  resources :posts, only: [:index, :show]
-  end
+ resources :users, only: [:index, :show] do
+   resources :posts, only: [:index, :show, :new, :create] do
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
+   end
+ end
 
 end
