7175cf1d9f92315aa7d7d55ae217573fef2a2f67
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-05-23 10:05:28 +0100

    fix linter error

--- spec/view_spec/post_index_spec.rb
+++ spec/view_spec/post_index_spec.rb
@@ -11,37 +11,48 @@ RSpec.describe 'Render posts index page', type: :feature do
     Comment.create(text: 'You know Rails', author_id: @user.id, post_id: @first_post.id)
     visit user_posts_path(@user)
   end
+
   scenario 'should render user profile photo' do
     expect(page).to have_selector("img[src='#{@user.photo}']")
   end
+
   scenario 'should display user name' do
     expect(page).to have_content(@user.name)
   end
+
   scenario 'shows number of user posts' do
     expect(page).to have_content(@user.posts.count)
   end
+
   scenario 'render button section for Pagination' do
     expect(page).to have_button('Pagination')
   end
+
   scenario 'redirects to the post show page' do
     click_link('a', match: :first)
     expect(page).to have_content(@first_post.title)
   end
+
   scenario 'displays post text' do
     expect(page).to have_text('Beautiful post', wait: 10)
   end
+
   scenario 'display the post title' do
     expect(page).to have_content(@first_post.title)
   end
+
   scenario 'display the post body' do
     expect(page).to have_content(@first_post.text)
   end
+
   scenario 'display the first comment on a post' do
     expect(page).to have_content('Hi Rita!')
   end
+
   scenario 'display the how many comments' do
     expect(page).to have_content(/comments: 3/i)
   end
+
   scenario 'display the how many Likes' do
     expect(page).to have_content("Likes: #{@first_post.LikesCounter}")
   end
